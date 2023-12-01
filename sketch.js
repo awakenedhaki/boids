a21a001ee393152953aa12715c377d9a7acb66ab
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-01 14:33:58 -0800

    Call update and show methods

--- sketch.js
+++ sketch.js
@@ -4,10 +4,13 @@ let swarm;
 // p5js Boilerplate ============================================================
 function setup() {
   createCanvas(1000, 500);
+  swarm = Swarm.initialize(400);
 }
 
 function draw() {
   background(0);
+  swarm.update();
+  swarm.show();
 }
 
 function keyPressed() {
