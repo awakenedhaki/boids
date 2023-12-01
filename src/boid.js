708fbafe84701cfe878dc8fa4e9e5049590a357c
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-01 11:34:54 -0800

    Implement Boid class boilerplate

--- src/boid.js
+++ src/boid.js
@@ -1,3 +1,49 @@
 class Boid {
-  constructor(x, y) {}
+  constructor(x, y) {
+    // Constants
+    this.MINT_GREEN = [201, 237, 220, 190];
+    this.RADIUS = 10;
+
+    // Behaviours
+    this.acceleration = createVector(0, 0);
+    this.velocity = p5.Vector.random2D();
+    this.position = createVector(x, y);
+
+    // Steering
+    this._separation = createVector(0, 0);
+    this._alignment = createVector(0, 0);
+    this._coherence = createVector(0, 0);
+  }
+
+  // Steering
+  applySteering() {
+    this.acceleration.add(this._separation);
+    this.acceleration.add(this._alignment);
+    this.acceleration.add(this._coherece);
+  }
+
+  separate(neighbours) {}
+
+  align(neighbours) {}
+
+  coehere(neighbours) {}
+
+  // Boilerplate
+  update() {
+    this.position.add(this.velocity);
+  }
+
+  show() {
+    fill(...this.MINT_GREEN);
+    ellipse(this.x, this.y, this.RADIUS);
+  }
+
+  // Getters/Setters
+  get x() {
+    return this.position.x;
+  }
+
+  get y() {
+    return this.position.y;
+  }
 }
