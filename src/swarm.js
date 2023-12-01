c7672bc91a271a8fe47153118bd9843bccbe4866
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-01 11:26:54 -0800

    Implement Swarm class: a collection of boids

--- src/swarm.js
+++ src/swarm.js
@@ -2,4 +2,30 @@ class Swarm {
   constructor(boids) {
     this.boids = boids;
   }
+
+  static initialize(n) {
+    const boids = [];
+    for (let i = 0; i < n; i++) {
+      const x = random(width);
+      const y = random(height);
+      boids.push(new Boid(x, y));
+    }
+    return new Swarm(boids);
+  }
+
+  // Boilerplate
+  update() {
+    this.boids.forEach((boid) => {
+      const neighbours = boid.findNeighbours(this.boids);
+
+      boid.calculateSteeringForces(neighbours);
+      boid.applyForces();
+
+      boid.update();
+    });
+  }
+
+  show() {
+    this.boids.forEach((boid) => boid.show());
+  }
 }
