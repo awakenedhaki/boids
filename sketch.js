bd7db6caca753448765b1ad50b93d8a5755bd0e2
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-02 02:58:25 -0800

    Set navigator in setup

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ let flock;
 // p5js Boilerplate ============================================================
 function setup() {
   createCanvas(1000, 500);
+  const navigator = new Navigator(
+    [alignment, separation, coherence],
+    [1, 1, 1]
+  );
   flock = Flock.initialize(400);
 }
 
