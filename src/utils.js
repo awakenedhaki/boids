10f73bd62dee926e123654cd4255e7f3b0ddc2a7
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-02 03:09:22 -0800

    Filter on boid collection attribute of flock

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 function findNeighbours(boid, flock, threshold) {
-  return flock.filter((agent) => {
+  return flock.boids.filter((agent) => {
     if (boid !== agent) {
       const distance = p5.Vector.dist(boid.position, agent.position);
       return distance < threshold;
