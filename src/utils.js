66cb5b6e93b72f8918960f2704e40189b2926f52
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-02 03:20:13 -0800

    Flock represents an array of boids

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,8 @@
 function findNeighbours(boid, flock, threshold) {
-  return flock.boids.filter((agent) => {
+  return flock.filter((agent) => {
     if (boid !== agent) {
       const distance = p5.Vector.dist(boid.position, agent.position);
       return distance < threshold;
-    } else {
-      return false;
     }
   });
 }
