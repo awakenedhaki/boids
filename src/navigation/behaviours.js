d90872bf11ba912bcf78cf347bf00155b648c910
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-02 02:52:28 -0800

    Implement steering forces

--- src/navigation/behaviours.js
+++ src/navigation/behaviours.js
@@ -1,8 +1,45 @@
-function alignment() {}
+function alignment(flock) {
+  if (flock.length === 0) {
+    return createVector(0, 0);
+  }
 
-function separation() {}
+  const alignmentSteering = flock.reduce(
+    (acc, agent) => acc.add(agent.velocity),
+    createVector(0, 0)
+  );
 
-function coherence() {}
+  alignmentSteering.div(flock.length);
+  return alignmentSteering;
+}
+
+function separation(boid, flock) {
+  if (flock.length === 0) {
+    return createVector(0, 0);
+  }
+
+  const immenentNeighbours = findNeighbours(boid, flock, boid.AVOIDACNE_RADIUS);
+  const collisionAvoidanceSteering = immenentNeighbours.reduce(
+    (acc, agent) => acc.add(p5.Vector(boid.position, agent.position)),
+    createVector(0, 0)
+  );
+
+  collisionAvoidanceSteering.div(immenentNeighbours.length);
+  return collisionAvoidanceSteering;
+}
+
+function coherence(boid, flock) {
+  if (flock.length === 0) {
+    return createVector(0, 0);
+  }
+
+  const cohesionSteering = flock.reduce(
+    (acc, agent) => acc.add(agent.position),
+    createVector(0, 0)
+  );
+
+  cohesionSteering.div(flock.length);
+  return cohesionSteering;
+}
 
 function smoothCoherence() {}
 
